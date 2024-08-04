547175e1a9eb42c2059f72173e802106d36cbcba
Govinda-Rathore <govindarathore431@gmail.com>
2024-08-04 20:44:11 +0530

    Hi there
    Adding new files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        'col-1':'0,56,67',
+        'col-2':'30,34,45'
+      },
+    },
+  },
+  plugins: [],
+}
+
